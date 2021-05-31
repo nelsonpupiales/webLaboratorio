9a8fdffb4d288b6edbb586aa8f2335604c4de899
Nelson Pupiales <nelsonpupiales@gmail.com>
2021-05-31 09:45:29 -0500

    modificacion server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require ('express');
-const app=express();
 
-app.use(express.static(_dirname + '/dist/web'));
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.listen(process.env.PORT || 8080);
+// Serve static files
+app.use(express.static(__dirname + '/dist/web'));
 
-app.get('/*', function(req, res){
-    res.sendFile(path.join(_dirname + '/dist/web/index.html'));
-})
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/web/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
 
 console.log('Consoles listening');
