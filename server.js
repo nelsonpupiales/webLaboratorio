7c361bff4ac14ba72064a085a2be93990eeb4a3f
Nelson Pupiales <nelsonpupiales@gmail.com>
2021-05-31 11:39:03 -0500

    modificacion server

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const express = require ('express');
-const app=express();
-
-app.use(express.static(_dirname + '/dist/'));
-
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res){
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
-})
-
-console.log('Consoles listening');
